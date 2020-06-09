f503b9eab8c977f47dfabae55112e754867cb6a4
Matthias Osswald <mat.osswald@sap.com>
2020-06-09 17:10:53 +0200

    [FIX] TypeScript type definition support
    Lazy-loading breaks the automatic type definition detection from
    TypeScript. Therefore we need to manually declare the exported types.

--- index.js
+++ index.js
@@ -2,43 +2,73 @@
  * @module @ui5/fs
  * @public
  */
-const modules = {
+module.exports = {
 	/**
 	 * @public
 	 * @alias module:@ui5/fs.adapters
 	 * @namespace
 	 */
 	adapters: {
+		/**
+		 * @type {typeof import('./lib/adapters/AbstractAdapter')}
+		 */
 		AbstractAdapter: "./lib/adapters/AbstractAdapter",
+		/**
+		 * @type {typeof import('./lib/adapters/FileSystem')}
+		 */
 		FileSystem: "./lib/adapters/FileSystem",
+		/**
+		 * @type {typeof import('./lib/adapters/Memory')}
+		 */
 		Memory: "./lib/adapters/Memory"
 	},
+	/**
+	 * @type {typeof import('./lib/AbstractReader')}
+	 */
 	AbstractReader: "./lib/AbstractReader",
+	/**
+	 * @type {typeof import('./lib/AbstractReaderWriter')}
+	 */
 	AbstractReaderWriter: "./lib/AbstractReaderWriter",
+	/**
+	 * @type {typeof import('./lib/DuplexCollection')}
+	 */
 	DuplexCollection: "./lib/DuplexCollection",
+	/**
+	 * @type {import('./lib/fsInterface')}
+	 */
 	fsInterface: "./lib/fsInterface",
+	/**
+	 * @type {typeof import('./lib/ReaderCollection')}
+	 */
 	ReaderCollection: "./lib/ReaderCollection",
+	/**
+	 * @type {typeof import('./lib/ReaderCollectionPrioritized')}
+	 */
 	ReaderCollectionPrioritized: "./lib/ReaderCollectionPrioritized",
+	/**
+	 * @type {typeof import('./lib/Resource')}
+	 */
 	Resource: "./lib/Resource",
+	/**
+	 * @type {import('./lib/resourceFactory')}
+	 */
 	resourceFactory: "./lib/resourceFactory"
 };
 
-const hasOwnProperty = Object.prototype.hasOwnProperty;
 function exportModules(exportRoot, modulePaths) {
-	for (const moduleName in modulePaths) {
-		if (hasOwnProperty.call(modulePaths, moduleName)) {
-			if (typeof modulePaths[moduleName] === "object") {
-				exportRoot[moduleName] = {};
-				exportModules(exportRoot[moduleName], modulePaths[moduleName]);
-			} else {
-				Object.defineProperty(exportRoot, moduleName, {
-					get() {
-						return require(modulePaths[moduleName]);
-					}
-				});
-			}
+	for (const moduleName of Object.keys(modulePaths)) {
+		if (typeof modulePaths[moduleName] === "object") {
+			exportRoot[moduleName] = {};
+			exportModules(exportRoot[moduleName], modulePaths[moduleName]);
+		} else {
+			Object.defineProperty(exportRoot, moduleName, {
+				get() {
+					return require(modulePaths[moduleName]);
+				}
+			});
 		}
 	}
 }
 
-exportModules(module.exports, modules);
+exportModules(module.exports, JSON.parse(JSON.stringify(module.exports)));
