9453e4829f503c74fd9985b1633b64057153615d
Merlin Beutlberger <m.beutlberger@sap.com>
2021-12-22 17:10:20 +0100

    [INTERNAL] Transformer: Callback can request resource for modification

--- test/lib/readers/Transformer.js
+++ test/lib/readers/Transformer.js
@@ -5,6 +5,9 @@ const Transformer = require("../../../lib/readers/Transformer");
 function getDummyResource(name) {
 	return {
 		name,
+		getPath: function() {
+			return `/resources/${name}`;
+		},
 		clone: function() {
 			return getDummyResource(name);
 		}
@@ -12,8 +15,8 @@ function getDummyResource(name) {
 }
 
 test("_byGlob: Basic transformation", async (t) => {
-	const resourceA = getDummyResource("resource a");
-	const resourceB = getDummyResource("resource b");
+	const resourceA = getDummyResource("resource.a");
+	const resourceB = getDummyResource("resource.b");
 	const abstractReader = {
 		_byGlob: sinon.stub().returns(Promise.resolve([resourceA, resourceB]))
 	};
@@ -22,23 +25,24 @@ test("_byGlob: Basic transformation", async (t) => {
 	};
 	const readerCollection = new Transformer({
 		reader: abstractReader,
-		callback: function(resource) {
-			if (resource.name === "resource a") {
-				resource.name = "transformed resource a";
+		callback: async function(resourcePath, getClonedResource) {
+			if (resourcePath === "/resources/resource.a") {
+				const resource = await getClonedResource();
+				resource.name = "transformed resource.a";
 			}
 		}
 	});
 
 	const resources = await readerCollection._byGlob("anyPattern", {}, trace);
 	t.deepEqual(resources.length, 2, "Still two resources in result set");
-	t.deepEqual(resources[0].name, "transformed resource a", "resource a has been transformed in result");
-	t.deepEqual(resources[1].name, "resource b", "resource b has not been transformed");
-	t.not(resources[1], resourceB, "resource b instance has been cloned");
-	t.deepEqual(resourceA.name, "resource a", "Original resource a has not been transformed");
+	t.deepEqual(resources[0].name, "transformed resource.a", "resource.a has been transformed in result");
+	t.deepEqual(resources[1].name, "resource.b", "resource.b has not been transformed");
+	t.is(resources[1], resourceB, "resource.b instance has not been cloned");
+	t.deepEqual(resourceA.name, "resource.a", "Original resource.a has not been transformed");
 });
 
 test("_byPath: Basic transformation", async (t) => {
-	const resourceA = getDummyResource("resource a");
+	const resourceA = getDummyResource("resource.a");
 	const abstractReader = {
 		_byPath: sinon.stub().returns(resourceA)
 	};
@@ -47,19 +51,20 @@ test("_byPath: Basic transformation", async (t) => {
 	};
 	const readerCollection = new Transformer({
 		reader: abstractReader,
-		callback: function(resource) {
-			resource.name = "transformed resource a";
+		callback: async function(resourcePath, getClonedResource) {
+			const resource = await getClonedResource();
+			resource.name = "transformed resource.a";
 		}
 	});
 
 	const resource = await readerCollection._byPath("anyPattern", {}, trace);
 
-	t.deepEqual(resource.name, "transformed resource a", "resource a has been transformed in result");
-	t.deepEqual(resourceA.name, "resource a", "Original resource a has not been transformed");
+	t.deepEqual(resource.name, "transformed resource.a", "resource.a has been transformed in result");
+	t.deepEqual(resourceA.name, "resource.a", "Original resource.a has not been transformed");
 });
 
 test("_byPath: No transformation", async (t) => {
-	const resourceB = getDummyResource("resource b");
+	const resourceB = getDummyResource("resource.b");
 	const abstractReader = {
 		_byPath: sinon.stub().returns(resourceB)
 	};
@@ -68,90 +73,12 @@ test("_byPath: No transformation", async (t) => {
 	};
 	const readerCollection = new Transformer({
 		reader: abstractReader,
-		callback: function(resource) {
+		callback: async function(resourcePath, getClonedResource) {
 			return;
 		}
 	});
 
 	const resource = await readerCollection._byPath("anyPattern", {}, trace);
-	t.deepEqual(resource.name, "resource b", "Correct resource in result");
-	t.not(resource, resourceB, "resource b instance has been cloned");
-});
-
-
-test("async _byGlob: Basic transformation", async (t) => {
-	const resourceA = getDummyResource("resource a");
-	const resourceB = getDummyResource("resource b");
-	const abstractReader = {
-		_byGlob: sinon.stub().returns(Promise.resolve([resourceA, resourceB]))
-	};
-	const trace = {
-		collection: sinon.spy()
-	};
-	const readerCollection = new Transformer({
-		reader: abstractReader,
-		callback: function(resource) {
-			return new Promise((resolve) => {
-				setTimeout(() => {
-					if (resource.name === "resource a") {
-						resource.name = "transformed resource a";
-					}
-					resolve();
-				}, 10);
-			});
-		}
-	});
-
-	const resources = await readerCollection._byGlob("anyPattern", {}, trace);
-	t.deepEqual(resources.length, 2, "Still two resources in result set");
-	t.deepEqual(resources[0].name, "transformed resource a", "resource a has been transformed in result");
-	t.deepEqual(resources[1].name, "resource b", "resource b has not been transformed");
-	t.not(resources[1], resourceB, "resource b instance has been cloned");
-	t.deepEqual(resourceA.name, "resource a", "Original resource a has not been transformed");
-});
-
-test("async _byPath: Basic transformation", async (t) => {
-	const resourceA = getDummyResource("resource a");
-	const abstractReader = {
-		_byPath: sinon.stub().returns(resourceA)
-	};
-	const trace = {
-		collection: sinon.spy()
-	};
-	const readerCollection = new Transformer({
-		reader: abstractReader,
-		callback: function(resource) {
-			return new Promise((resolve) => {
-				setTimeout(() => {
-					resource.name = "transformed resource a";
-					resolve();
-				}, 10);
-			});
-		}
-	});
-
-	const resource = await readerCollection._byPath("anyPattern", {}, trace);
-
-	t.deepEqual(resource.name, "transformed resource a", "resource a has been transformed in result");
-	t.deepEqual(resourceA.name, "resource a", "Original resource a has not been transformed");
-});
-
-test("async _byPath: No transformation", async (t) => {
-	const resourceB = getDummyResource("resource b");
-	const abstractReader = {
-		_byPath: sinon.stub().returns(resourceB)
-	};
-	const trace = {
-		collection: sinon.spy()
-	};
-	const readerCollection = new Transformer({
-		reader: abstractReader,
-		callback: async function(resource) {
-			return;
-		}
-	});
-
-	const resource = await readerCollection._byPath("anyPattern", {}, trace);
-	t.deepEqual(resource.name, "resource b", "Correct resource in result");
-	t.not(resource, resourceB, "resource b instance has been cloned");
+	t.deepEqual(resource.name, "resource.b", "Correct resource in result");
+	t.is(resource, resourceB, "resource.b instance has not been cloned");
 });
