6320c4c8842252f09e577c22c872a7b282721f1b
Yavor Ivanov <yavor.ivanov@sap.com>
2024-08-27 10:32:44 +0300

    refactor: Migrate fsInterface

--- src/fsInterface.ts
+++ src/fsInterface.ts
@@ -1,11 +1,30 @@
-function toPosix(inputPath) {
+import AbstractReader from "./AbstractReader.js";
+import type * as fs from "node:fs";
+import {Buffer} from "node:buffer";
+
+function toPosix(inputPath: string) {
 	return inputPath.replace(/\\/g, "/");
 }
+
+// Define the types for the options parameter
+type Read_File_Options = {encoding?: string} | string | undefined;
+
+// Define the types for the callback functions
+type Read_File_Callback = (err: Error | null, data?: Buffer | string) => void;
+type Stat_Callback = (err: Error | null, stats?: fs.Stats) => void;
+type Readdir_Callback = (err: Error | null, files?: string[]) => void;
+type Mkdir_Callback = (err?: Error | null) => void;
 
 /**
  * @public
  * @module @ui5/fs/fsInterface
  */
+interface File_System {
+	readFile: (fsPath: string, options: Read_File_Options, callback: Read_File_Callback) => void;
+	stat: (fsPath: string, callback: Stat_Callback) => void;
+	readdir: (fsPath: string, callback: Readdir_Callback) => void;
+	mkdir: (fsPath: string, callback: Mkdir_Callback) => void;
+};
 
 /**
  * Wraps readers to access them through a [Node.js fs]{@link https://nodejs.org/api/fs.html} styled interface.
@@ -21,8 +40,8 @@ function toPosix(inputPath) {
  * [<code>readdir</code>]{@link https://nodejs.org/api/fs.html#fs_fs_readdir_path_options_callback} and
  * [<code>mkdir</code>]{@link https://nodejs.org/api/fs.html#fs_fs_mkdir_path_options_callback}
  */
-function fsInterface(reader) {
-	return {
+function fsInterface(reader: AbstractReader) {
+	const fileSystem: File_System = {
 		readFile(fsPath, options, callback) {
 			if (typeof options === "function") {
 				callback = options;
@@ -57,7 +76,7 @@ function fsInterface(reader) {
 		},
 		stat(fsPath, callback) {
 			const posixPath = toPosix(fsPath);
-			reader.byPath(posixPath, {
+			void reader.byPath(posixPath, {
 				nodir: false,
 			}).then(function (resource) {
 				if (!resource) {
@@ -65,17 +84,17 @@ function fsInterface(reader) {
 					error.code = "ENOENT"; // "File or directory does not exist"
 					callback(error);
 				} else {
-					callback(null, resource.getStatInfo());
+					callback(null, resource.getStatInfo() as fs.StatsBase<number>);
 				}
 			}).catch(callback);
 		},
 		readdir(fsPath, callback) {
 			let posixPath = toPosix(fsPath);
-			if (!posixPath.match(/\/$/)) {
+			if (!(/\/$/.exec(posixPath))) {
 				// Add trailing slash if not present
 				posixPath += "/";
 			}
-			reader.byGlob(posixPath + "*", {
+			void reader.byGlob(posixPath + "*", {
 				nodir: false,
 			}).then((resources) => {
 				const files = resources.map((resource) => {
@@ -84,9 +103,11 @@ function fsInterface(reader) {
 				callback(null, files);
 			}).catch(callback);
 		},
-		mkdir(fsPath, callback) {
+		mkdir(_fsPath, callback) {
 			setTimeout(callback, 0);
 		},
 	};
+
+	return fileSystem;
 }
 export default fsInterface;
