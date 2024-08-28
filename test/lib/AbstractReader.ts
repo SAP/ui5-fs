2b34270ad220f1cb912f1b139528c08a059f2b67
Max Reichmann <max.reichmann@sap.com>
2024-08-28 11:25:29 +0200

    refactor: Fix some ESLint errors

--- test/lib/AbstractReader.ts
+++ test/lib/AbstractReader.ts
@@ -14,22 +14,22 @@ test("Incomplete AbstractReader subclass: Abstract functions throw error", (t) =
 	class Dummy extends AbstractReader {}
 
 	const instance = new Dummy();
-	t.throws(() => {
-		instance._byGlob();
+	t.throws(async () => {
+		await instance._byGlob();
 	}, {
 		instanceOf: Error,
 		message: "Function '_byGlob' is not implemented",
 	});
 
-	t.throws(() => {
-		instance._runGlob();
+	t.throws(async () => {
+		await instance._runGlob();
 	}, {
 		instanceOf: Error,
 		message: "Function '_runGlob' is not implemented",
 	});
 
-	t.throws(() => {
-		instance._byPath();
+	t.throws(async () => {
+		await instance._byPath();
 	}, {
 		instanceOf: Error,
 		message: "Function '_byPath' is not implemented",
